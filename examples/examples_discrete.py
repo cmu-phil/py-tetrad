495d3a0a9d11d7eab9ed021009bd38bf3523f5b7
Bryan Andrews <bja43@pitt.edu>
2023-02-26 15:40:55 -0600

    Update examples_discrete.py

--- examples/examples_discrete.py
+++ examples/examples_discrete.py
@@ -13,7 +13,6 @@ jpype.startJVM(classpath=[f"{BASE_DIR}/tetrad-gui-7.2.2-launch.jar"])
 import pandas as pd
 import pytetrad.translate as tr
 
-import java.util as util
 import edu.cmu.tetrad.search as ts
 
 
@@ -24,37 +23,40 @@ print(data)
 
 variables = data.getVariables()
 
-score = ts.BicScore(data)
+score = ts.SemBicScoreDGWrapper(data)
 score.setPenaltyDiscount(2)
 score.setStructurePrior(0)
+
+test = ts.IndTestScore(score, data)
+test.setAlpha(0.01)
 
 print("\nfGES\n")
 fges = ts.Fges(score)
 fges_graph = fges.search()
 
-# print("\nBOSS\n")
-# boss = ts.Boss(score)
-# boss.setUseDataOrder(False)
-# boss.setNumStarts(5)
-# boss.bestOrder(variables)
-# boss_graph = boss.getGraph(True)
+print("\nBOSS\n")
+boss = ts.Boss(test, score)
+boss.setUseDataOrder(False)
+boss.setNumStarts(1)
+boss.bestOrder(variables)
+boss_graph = boss.getGraph(True)
 
-# print("\nGRaSP\n")
-# grasp = ts.Grasp(score)
-# grasp.setOrdered(False)
-# grasp.setUseDataOrder(False)
-# grasp.setNumStarts(5)
-# grasp.bestOrder(variables)
-# grasp_graph = grasp.getGraph(True)
+print("\nGRaSP\n")
+grasp = ts.Grasp(test, score)
+grasp.setOrdered(False)
+grasp.setUseDataOrder(False)
+grasp.setNumStarts(5)
+grasp.bestOrder(variables)
+grasp_graph = grasp.getGraph(True)
 
 print(fges_graph)
-# print(boss_graph)
-# print(grasp_graph)
+print(boss_graph)
+print(grasp_graph)
 
 print(tr.tetrad_graph_to_pcalg(fges_graph))
-# print(tr.tetrad_graph_to_pcalg(boss_graph))
-# print(tr.tetrad_graph_to_pcalg(grasp_graph))
+print(tr.tetrad_graph_to_pcalg(boss_graph))
+print(tr.tetrad_graph_to_pcalg(grasp_graph))
 
 print(tr.tetrad_graph_to_causal_learn(fges_graph))
-# print(tr.tetrad_graph_to_causal_learn(boss_graph))
-# print(tr.tetrad_graph_to_causal_learn(grasp_graph))
+print(tr.tetrad_graph_to_causal_learn(boss_graph))
+print(tr.tetrad_graph_to_causal_learn(grasp_graph))
