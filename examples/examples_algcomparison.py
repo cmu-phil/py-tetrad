19ca8ba2b9fec5d973cdaf8d832ca9f0629d9265
jdramsey <jdramsey@andrew.cmu.edu>
2023-02-27 19:47:37 -0500

    Adding an algcomparison example usign JPype.

--- examples/examples_algcomparison.py
+++ examples/examples_algcomparison.py
@@ -8,7 +8,9 @@ sys.path.append(BASE_DIR)
 from pytetrad.util import startJVM
 startJVM()
 
-import pandas as pd
+# This is an example of how ot write and algcomparison script to do algorithm
+# comparison in Tetrad. It may not be the best example yet, but it does make
+# clear how the script can be written. JR 2023-02-27
 
 import edu.cmu.tetrad.util as tu
 
