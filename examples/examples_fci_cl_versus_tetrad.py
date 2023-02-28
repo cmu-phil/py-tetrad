998ca7c164472eaa4665d73d4f1e3fcad8c3201d
jdramsey <jdramsey@andrew.cmu.edu>
2023-02-28 01:49:28 -0500

    Added a comparison of CL FCI to Tetrad FCI..

--- examples/examples_fci_cl_versus_tetrad.py
+++ examples/examples_fci_cl_versus_tetrad.py
@@ -37,3 +37,44 @@ tetrad_fci_graph = tetrad_fci.search()
 print(tetrad_fci_graph)
 
 # These differ by one arrowhead, missing R3 rule application
+#
+# CL FCI
+#
+# Depth=3, working on node 5: 100%|██████████| 6/6 [00:00<00:00, 1104.78it/s]
+# Graph Nodes:
+# Frequency;Attack;Chord;Velocity;Displacement;Pressure
+#
+# Graph Edges:
+# 1. Frequency o-> Attack
+# 2. Frequency o-o Velocity
+# 3. Frequency o-> Pressure
+# 4. Attack <-> Chord
+# 5. Velocity o-> Attack
+# 6. Displacement o-> Attack
+# 7. Chord o-o Displacement
+# 8. Chord <-> Pressure
+# 9. Velocity o-> Pressure
+# 10. Displacement o-> Pressure
+#
+#
+# Tetrad FCI
+#
+# new thread
+# Starting BK Orientation.
+# Finishing BK Orientation.
+# Starting BK Orientation.
+# Finishing BK Orientation.
+# Graph Nodes:
+# Frequency;Attack;Chord;Velocity;Displacement;Pressure
+#
+# Graph Edges:
+# 1. Attack <-> Chord
+# 2. Chord <-> Pressure
+# 3. Displacement o-> Attack
+# 4. Displacement o-> Chord
+# 5. Displacement o-> Pressure
+# 6. Frequency o-> Attack
+# 7. Frequency o-> Pressure
+# 8. Frequency o-o Velocity
+# 9. Velocity o-> Attack
+# 10. Velocity o-> Pressure
