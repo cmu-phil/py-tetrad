24b1d9b343a7f37f90a8e352f2330e6cbef903d6
jdramsey <jdramsey@andrew.cmu.edu>
2023-02-28 01:39:43 -0500

    Added a comparison of CL FCI to Tetrad FCI..

--- examples/examples_fci_cl_versus_tetrad.py
+++ examples/examples_fci_cl_versus_tetrad.py
@@ -39,4 +39,4 @@ tetrad_fci = ts.Fci(test)
 tetrad_fci_graph = tetrad_fci.search()
 print(tetrad_fci_graph)
 
-# These differ by one arrowhead.
+# These differ by one arrowhead, missing R3 rule application
