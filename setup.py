66f49af04fbfe9d629776d12c2f8351620558432
jdramsey <jdramsey@andrew.cmu.edu>
2023-03-07 14:28:34 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    README = fh.read()
+
+VERSION = '0.1'
+
+setuptools.setup(
+    name='py-tetrad',
+    version=VERSION,
+    author='',
+    description='py-tetrad Python Package',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'numpy',
+        'scipy',
+        'scikit-learn',
+        'graphviz',
+        'statsmodels',
+        'pandas',
+        'matplotlib',
+        'networkx',
+        'pydot',
+        'tqdm'
+    ],
+    url='https://github.com/cmu-phil/py-tetrad',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
