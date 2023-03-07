4142b2a43000aa6a05f9623eca02bfc497e7b3da
jdramsey <jdramsey@andrew.cmu.edu>
2023-03-07 16:50:53 -0500

    Moved examples inside the pytrad directory

--- examples/simulating_data_discrete.py
+++ examples/simulating_data_discrete.py
@@ -14,5 +14,5 @@ D, G = util.simulateDiscrete(num_meas=100, samp_size=1000)
 D.to_csv('../mydata.csv', index=False)
 
 # To save out causal learn graph:
-with open('../../mygraph.txt', 'w') as f:
+with open('../mygraph.txt', 'w') as f:
     f.write(str(G))
