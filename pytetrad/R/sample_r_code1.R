e0f127bb06b7d1a8efcab722e3c5f1cae0883967
jdramsey <jdramsey@andrew.cmu.edu>
2023-04-03 13:36:24 -0400

    Revising R code documentation.

--- pytetrad/R/sample_r_code1.R
+++ pytetrad/R/sample_r_code1.R
@@ -8,23 +8,8 @@
 ## can be converted into R data frames using the 'translate' commands.
 ## Any pandas data frame in Python will be immediately usable as a
 ## data frame in R and will require no further translation. Such
-## environment variables can be immediately used in R. Otherwise, 
+## environment variables can be immediately used in R. Otherwise,
 ## translating them into data frames does the trick.
-##
-## We're working out instructions for running this in RStudio.
-##
-## For Mac, it's seems necessary to start RStudio from the command line.
-## Also, if you change the Python scripts you're importing and want to
-## re-import them, you may have to quit RStudio and restart it; for some
-## reason reticulate seems to be unable to forget previous imports.
-##
-## Set JAVA_HOME to the location of Java, preferably in .bash_profile
-## and install RStudio if it's not already
-##
-## Also, install py-tetrad using directions in the README for this package.
-## Then in terminal:
-##
-## > open -na RStudio
 ##
 ## You will need to adjust this path to your path for py-tetrad.
 setwd("~/py-tetrad/pytetrad")
--- pytetrad/R/sample_r_code2.R
+++ pytetrad/R/sample_r_code2.R
@@ -7,19 +7,6 @@
 ## use arbitrary search methods with arbitrary scores, and passing in
 ## knowledge is easy.
 ##
-## For Mac, it's seems necessary to start RStudio from the command line.
-## Also, if you change the Python scripts you're importing and want to
-## re-import them, you may have to quit RStudio and restart it; for some
-## reason reticulate seems to be unable to forget previous imports.
-##
-## Set JAVA_HOME to the location of Java, preferably in .bash_profile
-## and install RStudio if it's not already.
-##
-## Also, install py-tetrad using directions in the README for this package.
-## Then in terminal:
-##
-## > open -na RStudio
-##
 ## You will need to adjust this path to your path for py-tetrad.
 setwd("~/py-tetrad/pytetrad")
 
