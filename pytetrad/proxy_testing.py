1c409019447b0f1fba99216f28782862e72aa5d2
Bryan Andrews <bja43@pitt.edu>
2023-04-17 12:10:24 -0500

    added an example running the airfoil example with bryan's super lame same (implemented in python)

---
+++ pytetrad/proxy_testing.py
@@ -0,0 +1,109 @@
+
+from jpype import JImplements, JOverride
+import jpype.imports
+
+PATH = "/Users/bryanandrews/Documents/py-tetrad/pytetrad"
+
+try:
+    jpype.startJVM(classpath=[f"{PATH}/resources/tetrad-gui-current-launch.jar"])
+except OSError:
+    pass
+    # print("JVM already started")
+
+import pandas as pd
+import numpy as np
+
+import tools.translate as tr
+
+import java.util as util
+import edu.cmu.tetrad.data as td
+import edu.cmu.tetrad.search as ts
+
+
+@JImplements(ts.Score)
+class Bsls:
+    def __init__(self, df):
+        self.df = df
+        self.n, self.p = df.shape
+        self.corr = df.corr().values
+        self.penalty = 2 * np.log(self.n) / self.n
+        self.variables = util.ArrayList()
+        self.variable_map = {}
+        for col in df.columns:
+            col = str(col)
+            variable = td.ContinuousVariable(col)
+            self.variables.add(variable)
+            self.variable_map[col] = variable
+
+
+    @JOverride
+    def localScore(self, *args):
+        if len(args) == 1: return 0
+        elif isinstance(args[1], int): S = [args[1]]
+        else: S = list(args[1])
+
+        score = -len(S) * self.penalty
+        score += np.linalg.slogdet(self.corr[np.ix_(S, S)])[1]
+        S.append(args[0])
+        score -= np.linalg.slogdet(self.corr[np.ix_(S, S)])[1]
+
+        return score
+
+    @JOverride
+    def localScoreDiff(self, *args):
+        node = args[0]
+        if len(args) == 2: parents = []
+        else: parents = list(args[2])
+
+        diff = -self.localScore(node, parents)
+        parents.append(args[1])
+        diff += self.localScore(node, parents)
+
+        return diff 
+    
+    @JOverride
+    def getVariables(self):
+        return self.variables
+    
+    @JOverride
+    def getSampleSize(self):
+        return self.n
+
+    @JOverride
+    def toString(self):
+        return "Bryan's Super Lame Score"
+
+    @JOverride
+    def getVariable(self, targetName):
+        if targetName in self.variable_map: 
+            return self.variable_map[targetName]
+        return None
+
+    @JOverride
+    def isEffectEdge(self, bump):
+        return False
+
+    @JOverride
+    def getMaxDegree(self):
+        return 1000
+
+    @JOverride
+    def defaultScore(self):
+        return self
+
+
+df = pd.read_csv(f"{PATH}/resources/airfoil-self-noise.continuous.txt", sep="\t")
+df = df.astype({col: "float64" for col in df.columns})
+
+score = Bsls(df)
+
+graph = ts.Fges(score).search()
+print('FGES w/ BSLS', graph)
+
+data = tr.pandas_data_to_tetrad(df)
+score = ts.SemBicScore(data)
+score.setPenaltyDiscount(1)
+score.setStructurePrior(0)
+
+graph = ts.Fges(score).search()
+print('FGES w/ SEM-BIC', graph)
