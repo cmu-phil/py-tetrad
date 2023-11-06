4601c23ecf2b2654a3c810a2f757584aee21872d
jdramsey <jdramsey@andrew.cmu.edu>
2023-11-06 11:09:10 -0500

    Deleting dead code and removing graph to causal-learn method since the causal-learn package alread has this transform.

--- pytetrad/tools/translate.py
+++ pytetrad/tools/translate.py
@@ -24,10 +24,6 @@ sys.path.append(BASE_DIR)
 
 import numpy as np
 import pandas as pd
-# from causallearn.graph.GeneralGraph import GeneralGraph
-# from causallearn.graph.GraphNode import GraphNode
-# from causallearn.graph.Endpoint import Endpoint
-# from causallearn.graph.Edge import Edge
 from pandas import DataFrame
 
 import java.util as util
@@ -112,50 +108,6 @@ def graph_to_matrix(g, nullEpt = 0, circleEpt = 1, arrowEpt = 2, tailEpt = 3):
         columns_.append(str(name))
 
     return pd.DataFrame(A, columns=columns_)
-
-# def tetrad_graph_to_pcalg(g):
-#     endpoint_map = {"NULL": 0,
-#                     "CIRCLE": 1,
-#                     "ARROW": 2,
-#                     "TAIL": 3}
-#
-#     nodes = g.getNodes()
-#     p = g.getNumNodes()
-#     A = np.zeros((p, p), dtype=int)
-#
-#     for edge in g.getEdges():
-#         i = nodes.indexOf(edge.getNode1())
-#         j = nodes.indexOf(edge.getNode2())
-#         A[j][i] = endpoint_map[edge.getEndpoint1().name()]
-#         A[i][j] = endpoint_map[edge.getEndpoint2().name()]
-#
-#     columns_ = []
-#
-#     for name in nodes:
-#         columns_.append(str(name))
-#
-#     return pd.DataFrame(A, columns=columns_)
-
-
-# def tetrad_graph_to_causal_learn(g):
-#     endpoint_map = {"TAIL": Endpoint.TAIL,
-#                     "NULL": Endpoint.NULL,
-#                     "ARROW": Endpoint.ARROW,
-#                     "CIRCLE": Endpoint.CIRCLE,
-#                     "STAR": Endpoint.STAR}
-#
-#     nodes = [GraphNode(str(node.getName())) for node in g.getNodes()]
-#     graph = GeneralGraph(nodes)
-#
-#     for edge in g.getEdges():
-#         node1 = graph.get_node(edge.getNode1().getName())
-#         node2 = graph.get_node(edge.getNode2().getName())
-#         endpoint1 = endpoint_map[edge.getEndpoint1().name()]
-#         endpoint2 = endpoint_map[edge.getEndpoint2().name()]
-#         graph.add_edge(Edge(node1, node2, endpoint1, endpoint2))
-#
-#     return graph
-
 
 # PASS ME A GraphViz Graph object and call it gdot!
 def write_gdot(g, gdot):
