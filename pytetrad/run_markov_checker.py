034be44a5079ec85bfec3d9d17326a1e80d4435e
jdramsey <jdramsey@andrew.cmu.edu>
2023-11-14 08:43:06 -0500

    Made a new module to show how to run the Markov Checker.

--- pytetrad/run_markov_checker.py
+++ pytetrad/run_markov_checker.py
@@ -67,5 +67,7 @@ p_ks_indep = mc.getKsPValue(True)
 fd_indep = mc.getFractionDependent(True)
 fd_dep = mc.getFractionDependent(False)
 
-print("Kolmogorov-Smirnov p-value Indep = ", p_ks_indep)
+p_exceeds_alpha = p_ks_indep > alpha
+
+print("Kolmogorov-Smirnov p-value Indep = ", p_ks_indep, "Uniform" if p_exceeds_alpha else "Non-Uniform")
 print("Fraction dependent for Indep = ", fd_indep, " fraction dependent Dep = ", fd_dep)
