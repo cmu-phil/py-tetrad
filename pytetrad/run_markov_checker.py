6c779e3193b786aef86f4d606e0dbd649586c27b
jdramsey <jdramsey@andrew.cmu.edu>
2023-11-14 05:47:58 -0500

    Made a new module to show how to run the Markov Checker.

--- pytetrad/run_markov_checker.py
+++ pytetrad/run_markov_checker.py
@@ -7,7 +7,7 @@ except OSError:
 
 import pandas as pd
 
-# Will wrap the Markov Checker in a nicer Python class at some point. -JR
+# We will wrap the Markov Checker in a nicer Python class at some point. -JR
 
 import tools.translate as tr
 import tools.TetradSearch as search
@@ -16,27 +16,40 @@ import edu.cmu.tetrad.search.ConditioningSetType as cst
 import edu.cmu.tetrad.algcomparison.independence as ind_
 from edu.cmu.tetrad.util import Params, Parameters
 
-
+# Load in the data and make sure all columns are being interpreted as continuous.
 data = pd.read_csv("resources/airfoil-self-noise.continuous.txt", sep="\t")
 data = data.astype({col: "float64" for col in data.columns})
 
-## Make a TetradSearch instance to run searches against. This helps to organize
-## the use of Tetrad search algorithms and hides the JPype code for those who
-## don't want to deal with it.
+# Make a TetradSearch instance to run searches against. This helps to organize
+# the use of Tetrad search algorithms and hides the JPype code for those who
+# don't want to deal with it.
 search = search.TetradSearch(data)
 search.set_verbose(False)
 
-## Pick the score to use, in this case a continuous linear, Gaussian score.
-search.use_sem_bic()
+# Pick the score to use, in this case a continuous linear, Gaussian score.
+search.use_sem_bic(penalty_discount=1)
 search.use_fisher_z(alpha=0.05)
 
-## Run various algorithms and print their results. For now (for compability with R)
+# Aun an algorithm and grab the CPCDAG
 print('BOSS')
 search.run_boss(num_starts=1, use_bes=True, time_lag=0, use_data_order=True)
 print(search.get_string())
-dag=search.get_dag_java()
+dag=search.get_java()
 print(dag)
 
+# Get the test used for the Markov Checker--this test will be used to look
+# to see whether p-values for conditional independence tests are distributed
+# as U(0, 1). We will also get a fraction dependent number, which should be
+# about equal to the alpha level of the test (if the p-values under the null
+# hypothesis of independence are distributed as U(0, 1)). We can test for this
+# Uniformity using a Kolmogorov-Smirnov test, comparing the actual
+# distriubtion of p-values to U(0, 1). (In the future other tests may be
+# added.)
+# 
+# We will also use it to check facts about the distribution for conditional 
+# dependencies; in this case, the fraction of depependence judgments should
+# be high, though not necessarily 1, since there may be some path
+# cancellations.
 params = Parameters()
 params.set(Params.ALPHA, 0.01)
 _test = ind_.FisherZ().getTest(tr.pandas_data_to_tetrad(data), params)
@@ -44,9 +57,8 @@ _test = ind_.FisherZ().getTest(tr.pandas_data_to_tetrad(data), params)
 mc = ts.MarkovCheck(dag, _test, cst.LOCAL_MARKOV)
 mc.generateResults()    
 p_ks_indep = mc.getKsPValue(True)
-print("ks p-value Indep = ", p_ks_indep)
-
 fd_indep = mc.getFractionDependent(True)
 fd_dep = mc.getFractionDependent(False)
 
+print("Kolmogorov-Smirnov p-value Indep = ", p_ks_indep)
 print("Fraction dependent for Indep = ", fd_indep, " fraction dependent Dep = ", fd_dep)
