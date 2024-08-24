4561a578c551b300e486595d72dba8e1816e7e73
jdramsey <jdramsey@andrew.cmu.edu>
2024-08-24 09:43:50 -0400

    Add WrappedClKci module
    Introduce WrappedClKci module to support optional KCI test integration using causal-learn in Markov Checker. This allows users to leverage the KCI test by installing causal-learn and uncommenting the necessary lines, improving test versatility.

--- pytetrad/run_markov_checker.py
+++ pytetrad/run_markov_checker.py
@@ -1,60 +1,34 @@
-# This script runs the Markov Checker on a dataset using the BOSS algorithm to find a CPDAG and the KCI test from
-# causal-learn, or Fisher Z, as the test for the Markov Checker. The Markov Checker is run in parallel. The script
-# also prints out the results of the Markov Checker, including the p-values for each conditional independence test,
-# the fraction of dependent judgments, and the number of tests run. -JR
+# This script runs BOSS to find a CPDAG and then runs the Markov Checker on the CPDAG, comparing to the sample data.
+# We show how to use either the Fisher Z test or the WrappedClKci test for the Markov Checker. The WrappedClKci test
+# is a wrapper around the KCI test from causal-learn. We don't import causal-learn in our scripts by default, as some
+# users cannot have it installed because its numerous dependencies. If you want to use the WrappedClKci test, you
+# will need to uncomment those lines in this script and install causal-learn.
 
 import jpype.imports
-from jpype import JImplements, JOverride
 
 try:
     jpype.startJVM(classpath=[f"resources/tetrad-current.jar"])
 except OSError:
     print("JVM already started")
 
-try:
-    from causallearn.utils.cit import CIT
-    from causallearn.search.ConstraintBased.PC import pc
-    from causallearn.utils.cit import kci
-except ImportError as e:
-    print('Could not import a causal-learn module: ', e)
-
 import pandas as pd
-
-import tools.translate as tr
 import tools.TetradSearch as search
-import edu.cmu.tetrad.data as td
-import edu.cmu.tetrad.graph as tg
-import edu.cmu.tetrad.search as ts
-import edu.cmu.tetrad.search.test as tt
-import edu.cmu.tetrad.util as util
-import edu.cmu.tetrad.algcomparison.independence as agind
-import java.util as ju
+# from tools import WrappedClKci as wc
 
-# Load in the data and make sure all columns are being interpreted as continuous.
-data = pd.read_csv("resources/airfoil-self-noise.continuous.txt",
-                   sep="\t")
+# data = pd.read_csv("resources/airfoil-self-noise.continuous.txt", sep="\t")
+data = pd.read_csv("resources/sample_data_joe.txt", sep="\t")
 data = data.astype({col: "float64" for col in data.columns})
 
-# Make a TetradSearch instance to run searches against. This helps to organize
-# the use of Tetrad search algorithms and hides the JPype code for those who
-# don't want to deal with it.
 _search = search.TetradSearch(data)
 
 # Hyperparameter settings
-num_starts = 1
-use_bes = True
-time_lag = 0
-use_data_order = True
-penalty_discount = 1
-alpha = 0.01
 
 # Pick the score to use, in this case a continuous linear, Gaussian score.
-_search.use_sem_bic(penalty_discount=penalty_discount)
+_search.use_sem_bic(penalty_discount=1)
 
 # Run an algorithm and grab the CPCDAG
 print('BOSS')
-_search.run_boss(num_starts=num_starts, use_bes=use_bes, time_lag=time_lag,
-                 use_data_order=use_data_order)
+_search.run_boss(num_starts=1, use_bes=True, time_lag=0, use_data_order=True)
 cpdag = _search.get_java()
 print(cpdag)
 
@@ -62,112 +36,16 @@ bic = cpdag.getAttribute("BIC")
 print("bic", bic)
 
 _search = search.TetradSearch(data)
-alpha_ = 0.01
 
-# We are giving the option to use Causal Leearn's KCI test for the Markov Checker. In order to do this, we need to
-# wrap the KCI test in a JPype object so that Tetrad can use it, and we need also to wrap this test as an
-# IndependenceWrapper using JPype as well. This is verbose, but if it works, the functionality can be wrapped up nicely.
+# We need to choose which test to use for the Markov checker--either Fisher Z or WrappedClKci.
 
-@JImplements(ts.IndependenceTest)
-class KciWrapper:
-    def __init__(self, df, alpha=alpha_):
-        self.df = df
-        self.data = df.values
-        self.alpha = alpha
-        self.kci_obj = CIT(self.data, "kci")
-
-        self.variables = ju.ArrayList()
-        self.variable_map = {}
-        self.reverse_variable_map = {}
-
-        for col in df.columns:
-            col = str(col)
-            variable = td.ContinuousVariable(col)
-            self.variables.add(variable)
-            self.variable_map[col] = variable
-            self.reverse_variable_map[variable] = df.columns.get_loc(col)
-
-    @JOverride
-    def checkIndependence(self, *args):
-        x = args[0]
-        y = args[1]
-        s = args[2]
-        fact = tg.IndependenceFact(x, y, s)
-
-        # Convert x, y, s to indices using the reverse map
-        X = self.reverse_variable_map[x]
-        Y = self.reverse_variable_map[y]
-        S = [self.reverse_variable_map[si] for si in s]
-
-        pValue = self.kci_obj(X, Y, S)
-        indep = pValue > self.alpha
-
-        result = tt.IndependenceResult(fact, indep, pValue, self.alpha - pValue)
-        return result
-
-    @JOverride
-    def getVariables(self, *arg):
-        return self.variables
-
-    @JOverride
-    def getData(self, *arg):
-        return tr.pandas_data_to_tetrad(self.df)
-
-    @JOverride
-    def isVerbose(self, *arg):
-        return False
-
-    @JOverride
-    def setVerbose(self, *arg):
-        pass
-
-    @JOverride
-    def toString(self, *arg):
-        return "KCI-Wrapper"
-
-    @JOverride
-    def getAlpha(self, *args):
-        return self.alpha
-
-
-@JImplements(agind.IndependenceWrapper)
-class WrappedClKci:
-
-    def __init__(self, df, alpha=alpha_):
-        self.df = df
-        self.alpha = alpha
-
-    @JOverride
-    def getTest(self, *args):
-        # pandas = tr.tetrad_data_to_pandas(args[0])
-        return KciWrapper(self.df)
-
-    @JOverride
-    def getDescription(self):
-        return "Wrapped CL KCI"
-
-    @JOverride
-    def getDataType(self):
-        return td.DataType.Continuous
-
-    @JOverride
-    def getParameters(self):
-        return util.ArrayList()
-
-
-# Now we need to choose which test to use for the Markov checker--either Fisher Z or WrappedClKci. We will use
-# WrappedClKci in this example.
-
-# Uncomment this line to use Fisher Z
 _search.use_fisher_z()
-
-# Uncomment this line to use WrappedClKci
-# _search.use_test(WrappedClKci(data))
+# _search.use_test(wc.WrappedClKci(data, alpha=0.01))
 
 # Note that we have added an option to control whether parallelism is used in the Markov Checker, as we are not
 # sure whether the Markov Checker is thread-safe when using the WrappedClKci test. We will test this in the future.
 ad_ind, ad_dep, bin_indep, bin_dep, frac_dep_ind, frac_dep_dep, num_tests_ind, num_tests_dep, mc = _search.markov_check(
-    cpdag, parallelized=True, sample_size=500)
+    cpdag, parallelized=True, sample_size=-1)
 
 print(f"AD p-value Indep = {ad_ind:5.4} Dep = {ad_dep:5.4}")
 print(f"Fraction dependent Indep = {frac_dep_ind:5.4} Dep = {frac_dep_dep:5.4}")
@@ -175,17 +53,12 @@ print(f"Num tests Indep = {num_tests_ind} Dep = {num_tests_dep}")
 
 results = mc.getResults(True)
 
+print("\nResults:\n")
+
 for result in results:
     print(result)
-    fact = result.getFact()
-    x = fact.getX()
-    y = fact.getY()
 
-    x_name = x.getName()
-    y_name = y.getName()
-
-    p_value = result.getPValue()
-
-    print('x = ', x_name)
-    print('y = ', y_name)
-    print('p-value = ', p_value)
+    # Can get the fact, p-value, etc. from the result object--e.g.:
+    # fact = result.getFact()
+    # p_value = result.getPValue()
+    # print(fact, 'p-value = ', p_value)
---
+++ pytetrad/tools/WrappedClKci.py
@@ -0,0 +1,123 @@
+# This module wraps the KCI test from causal-learn in a JPype object so that Tetrad can use it.
+# We are not assuming in this repository that causal-learn is installed, as we don't want to have the dependency
+# in our code (since some people can't have it there--it has dependencies that are unhelpful in certain circumstances).
+# So if you want to use the KCI test from causal-learn, you will need to install causal-learn yourself. (See the docs
+# for causal-learn for installation instructions.)
+#
+# To use WrappedClKci as a test in py-tetrad, you can do the following:
+# import tools/WrappedClKci as wc
+# ... load data into a pandas DataFrame df ...
+# test = wc.WrappedClKci(df, alpha=0.01)
+#
+# This test will implement the IndependenceWrapper interface in Tetrad. For a test that implements the
+# IndependenceTest interface in Tetrad, use KciWrapper instead, in this module.
+#
+# jdramsey 2024-08-24
+
+import jpype.imports
+from jpype import JImplements, JOverride
+
+try:
+    jpype.startJVM(classpath=[f"resources/tetrad-current.jar"])
+except OSError:
+    print("JVM already started")
+
+try:
+    from causallearn.utils.cit import CIT
+    from causallearn.search.ConstraintBased.PC import pc
+    from causallearn.utils.cit import kci
+except ImportError as e:
+    print('Could not import a causal-learn module: ', e)
+
+import tools.translate as tr
+import edu.cmu.tetrad.data as td
+import edu.cmu.tetrad.graph as tg
+import edu.cmu.tetrad.search as ts
+import edu.cmu.tetrad.search.test as tt
+import edu.cmu.tetrad.util as util
+import edu.cmu.tetrad.algcomparison.independence as agind
+import java.util as ju
+
+@JImplements(ts.IndependenceTest)
+class KciWrapper:
+    def __init__(self, df, alpha=0.01):
+        self.df = df
+        self.data = df.values
+        self.alpha = alpha
+        self.kci_obj = CIT(self.data, "kci")
+
+        self.variables = ju.ArrayList()
+        self.variable_map = {}
+        self.reverse_variable_map = {}
+
+        for col in df.columns:
+            col = str(col)
+            variable = td.ContinuousVariable(col)
+            self.variables.add(variable)
+            self.variable_map[col] = variable
+            self.reverse_variable_map[variable] = df.columns.get_loc(col)
+
+    @JOverride
+    def checkIndependence(self, *args):
+        x = args[0]
+        y = args[1]
+        s = args[2]
+        fact = tg.IndependenceFact(x, y, s)
+
+        # Convert x, y, s to indices using the reverse map
+        X = self.reverse_variable_map[x]
+        Y = self.reverse_variable_map[y]
+        S = [self.reverse_variable_map[si] for si in s]
+
+        pValue = self.kci_obj(X, Y, S)
+        indep = pValue > self.alpha
+
+        result = tt.IndependenceResult(fact, indep, pValue, self.alpha - pValue)
+        return result
+
+    @JOverride
+    def getVariables(self, *arg):
+        return self.variables
+
+    @JOverride
+    def getData(self, *arg):
+        return tr.pandas_data_to_tetrad(self.df)
+
+    @JOverride
+    def isVerbose(self, *arg):
+        return False
+
+    @JOverride
+    def setVerbose(self, *arg):
+        pass
+
+    @JOverride
+    def toString(self, *arg):
+        return "KCI-Wrapper"
+
+    @JOverride
+    def getAlpha(self, *args):
+        return self.alpha
+
+
+@JImplements(agind.IndependenceWrapper)
+class WrappedClKci:
+    def __init__(self, df, alpha=0.01):
+        self.df = df
+        self.alpha = alpha
+
+    @JOverride
+    def getTest(self, *args):
+        return KciWrapper(self.df, alpha=self.alpha)
+
+    @JOverride
+    def getDescription(self):
+        return "Wrapped CL KCI"
+
+    @JOverride
+    def getDataType(self):
+        return td.DataType.Continuous
+
+    @JOverride
+    def getParameters(self):
+        return util.ArrayList()
