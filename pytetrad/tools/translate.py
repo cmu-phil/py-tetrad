fb9fdced4fe577309c8c714d0f45d6dbeca82f64
Lizzie Silver <lizziesilver@gmail.com>
2024-11-17 20:37:06 +1100

    second attempt, importlib syntax

--- pytetrad/tools/translate.py
+++ pytetrad/tools/translate.py
@@ -7,7 +7,7 @@ import jpype.imports
 
 import os
 import importlib.resources as importlib_resources
-jar_path = importlib_resources.files('pytetrad').joinpath(['resources','tetrad-current.jar'])
+jar_path = importlib_resources.files('pytetrad').joinpath('resources','tetrad-current.jar')
 jar_path = str(jar_path)
 if not jpype.isJVMStarted():
     try:
