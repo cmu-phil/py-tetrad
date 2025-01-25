f2b0d35ed20ac9ed35fa54e93369156da4b088b9
jdramsey <jdramsey@andrew.cmu.edu>
2025-01-25 16:08:11 -0500

    Update example data, clustering, and latent names in mimbuild
    Replaced the dataset and expanded clustering and latent variable configurations to align with updated use cases. These changes enhance clarity and better demonstrate the mimbuild example functionality.

--- pytetrad/mimbuild_example.py
+++ pytetrad/mimbuild_example.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 # Load a dataset from a file
 # df = pd.read_csv("resources/airfoil-self-noise.continuous.txt", sep="\t")
-df = pd.read_csv("resources/sample_lng_data_10_2_1000.txt", sep="\t")
+df = pd.read_csv("resources/mimbuild_example_data.txt", sep="\t")
 df = df.astype({col: "float64" for col in df.columns})
 
 # Create a Mimbuild object.
@@ -15,13 +15,13 @@ mb = search.Mimbuild()
 cov = np.cov(df, rowvar=False)
 
 # Make a clustering. This is a list over lists of variable indices.
-clustering = [[0, 1, 2], [3, 4, 5]]
+clustering = [[0, 1, 2, 3, 4], [5, 6, 7, 8, 9], [10, 11, 12, 13, 14], [15, 16, 17, 18, 19], [20, 21, 22, 23, 24]]
 
 # Make a list of measure names. These are the variable names.
 measure_names = list(df.columns)
 
 # Make a list of latent variable names. These are the names associated with the above clusters.
-latent_names = ['L1', 'L2']
+latent_names = ['L1', 'L2', 'L3', 'L4', 'L5']
 
 # Do the search.
 graph = ts.mimbuild(clustering, measure_names, latent_names, cov)
