2334b62809d5d2b266d93efd5e0cbd617850392a
jdramsey <jdramsey@andrew.cmu.edu>
2025-02-19 13:15:04 -0500

    Refactor parameter handling in basis function methods.
    Renamed several methods for consistency and clarity, replacing "score" with "bic" in method names. Removed unused parameters `basis_type` and `basis_scale` to simplify method signatures. This change improves maintainability and ensures a more concise implementation.

--- pytetrad/tools/TetradSearch.py
+++ pytetrad/tools/TetradSearch.py
@@ -62,8 +62,8 @@ class TetradSearch:
     :ivar bootstrap_graphs: Stores results of any bootstrapped graph estimation run.
     :type bootstrap_graphs: object or None
     """
-    def __init__(self, data):
-        self.data = tr.pandas_data_to_tetrad(data)
+    def __init__(self, df):
+        self.data = tr.pandas_data_to_tetrad(df)
         self.score = None
         self.test = None
         self.java = None
@@ -132,35 +132,27 @@ class TetradSearch:
         self.SCORE = score_.DegenerateGaussianBicScore()
 
     # Uses covariance as a sufficient statistic
-    def use_basis_function_score(self, truncation_limit=3, basis_type=1, basis_scale=1, penalty_discount=2):
+    def use_basis_function_bic(self, truncation_limit=3, penalty_discount=2):
         self.params.set(Params.TRUNCATION_LIMIT, truncation_limit)
-        self.params.set(Params.BASIS_TYPE, basis_type)
-        self.params.set(Params.BASIS_SCALE, basis_scale)
         self.params.set(Params.PENALTY_DISCOUNT, penalty_discount)
         self.SCORE = score_.BasisFunctionBicScore()
 
     # Full sample.
-    def use_basis_function_score_fs(self, truncation_limit=3, basis_type=1, basis_scale=1, penalty_discount=2):
+    def use_basis_function_bic_fs(self, truncation_limit=3, penalty_discount=2):
         self.params.set(Params.TRUNCATION_LIMIT, truncation_limit)
-        self.params.set(Params.BASIS_TYPE, basis_type)
-        self.params.set(Params.BASIS_SCALE, basis_scale)
         self.params.set(Params.PENALTY_DISCOUNT, penalty_discount)
         self.SCORE = score_.BasisFunctionBicScoreTabular()
 
     # Uses covariance as a sufficient statistic.
-    def use_basis_function_lrt(self, truncation_limit=3, basis_type=1, basis_scale=1, alpha=0.01):
+    def use_basis_function_lrt(self, truncation_limit=3, alpha=0.01):
         self.params.set(Params.ALPHA, alpha)
         self.params.set(Params.TRUNCATION_LIMIT, truncation_limit)
-        self.params.set(Params.BASIS_TYPE, basis_type)
-        self.params.set(Params.BASIS_SCALE, basis_scale)
         self.TEST = ind_.BasisFunctionLrt()
 
     # Full sample
-    def use_basis_function_lrt_fs(self, truncation_limit=3, basis_type=1, basis_scale=1, alpha=0.01):
+    def use_basis_function_lrt_fs(self, truncation_limit=3, alpha=0.01):
         self.params.set(Params.ALPHA, alpha)
         self.params.set(Params.TRUNCATION_LIMIT, truncation_limit)
-        self.params.set(Params.BASIS_TYPE, basis_type)
-        self.params.set(Params.BASIS_SCALE, basis_scale)
         self.TEST = ind_.BasisFunctionLrtFullSample()
 
     def use_fisher_z(self, alpha=0.01, use_pseudoinverse=False):
