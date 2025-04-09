d60046aa3690a570af718080d8ca21280e5230d7
jdramsey <jdramsey@andrew.cmu.edu>
2025-04-09 08:37:24 -0400

    Add knowledge example script for using the pytetrad package
    This script demonstrates creating and using the Knowledge API with pytetrad, setting tiers, forbidden, and required edges. It also includes an example of running the FGES algorithm using a sample dataset.

---
+++ pytetrad/knowledge_example.py
@@ -0,0 +1,55 @@
+## This script assumes that the user has pip-installed the pytetrad package. Here is how:
+## pip install git+https://github.com/cmu-phil/py-tetrad
+
+# import graphviz as gviz
+import pandas as pd
+
+import importlib.resources as importlib_resources
+jar_path = importlib_resources.files('pytetrad').joinpath('resources','tetrad-current.jar')
+jar_path = str(jar_path)
+
+import pytetrad.tools.translate as tr
+import pytetrad.tools.translate as ptt
+import pytetrad.tools.visualize as ptv
+import edu.cmu.tetrad.graph as tg
+import edu.cmu.tetrad.search as ts
+import edu.cmu.tetrad.data as td
+import edu.cmu.tetrad.algcomparison.algorithm.multi as multi
+import edu.cmu.tetrad.util as util
+import java.util as jutil
+
+tiers = [['age', 'gender', 'height', 'weight', 'resting_heart', 'device', 'activity'],
+         ['steps', 'heart_rate', 'calories', 'distance']]
+
+df = pd.read_csv("resources/aw-fb-pruned18.data.mixed.numeric.txt", sep="\t")
+df = df[tiers[0] + tiers[1]]
+df = df.astype({col: int for col in ["gender", "device", "activity"]})
+
+# Knowledge API:
+# https://www.phil.cmu.edu/tetrad-javadocs/7.6.6/edu/cmu/tetrad/data/Knowledge.html
+
+knowledge = td.Knowledge()
+knowledge.setTierForbiddenWithin(0, True)
+for col in tiers[0]:
+    knowledge.addToTier(0, col)
+for col in tiers[1]:
+    knowledge.addToTier(1, col)
+
+knowledge.setForbidden("steps", "heart_rate")
+knowledge.setRequired("age", "gender")
+
+print()
+print('knowledge = ')
+print()
+print(knowledge)
+print()
+
+data = ptt.pandas_data_to_tetrad(df.sample(frac=1, replace=True))
+
+score = ts.score.DegenerateGaussianScore(data, True, 0.0)
+score.setPenaltyDiscount(2)
+
+alg = ts.Fges(score)
+alg.setKnowledge(knowledge)
+cpdag = alg.search()
+print(cpdag)
