1ec8cb612d76f74fc40684a36c1c6067d502d523
jdramsey <jdramsey@andrew.cmu.edu>
2025-04-09 08:16:05 -0400

    Disable debug output and set verbose mode to false.
    Commented out a debug print statement in `fofc_example.py` to clean up script output. Additionally, set `TetradSearch` verbose mode to false for less cluttered logs. Updated `tetrad-current.jar` to the latest version.

--- pytetrad/fofc_example.py
+++ pytetrad/fofc_example.py
@@ -7,9 +7,10 @@ import pytetrad.tools.TetradSearch as ts
 data = pd.read_csv("resources/fofc_data.txt", sep="\t")
 data = data.astype({col: "float64" for col in data.columns})
 
-print(data)
+# print(data)
 
 search = ts.TetradSearch(data)
+search.set_verbose(False)
 
 # tetrad_test is 1 for CCA, 2 Bollen-Ting 3 for Wishart
 search.run_fofc(include_structure_model=True, tetrad_test=1, alpha=0.001)
